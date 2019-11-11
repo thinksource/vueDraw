c6cd78bd95bb584006a74d5c68568b299afbdc5d
thinksource <foretribe@gmail.com>
2019-11-11 11:43:31 +1100

    change error handle place

--- src/components/InputArea.vue
+++ src/components/InputArea.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
+    <h2>Realtime draw SVG elements:</h2>
     <textarea v-model="desc" @change="txtChange" rows="8" cols="50"></textarea>
     <div>
       <h2>Errors in TextArea:</h2>
     </div>
     <ul id="alertList">
-      <li v-for="item in alertItem" :key="item">{{ item.toString() }}</li>
+      <li v-for="item in alertItems" :key="item">{{ item.toString() }}</li>
     </ul>
     <button @click="drawSVG">Draw SVG</button>
     <div id="SVGcontainer" ref="SVG">
@@ -22,12 +23,12 @@ import { it } from 'mocha'
 @Component
 export default class InputArea extends Vue {
   desc: string=''
-  alertItem: Array<SVGError>=[]
+  alertItems: Array<SVGError>=[]
   @Watch('desc')
   txtChange (value: string, oldValue: string) {
     if (typeof value === 'string') {
       let sItems = value.split(/\r?\n/)
-      this.alertItem = this.alertList(sItems)
+      this.alertItems = this.alertList(sItems)
     }
   }
 
@@ -35,13 +36,24 @@ export default class InputArea extends Vue {
     let DOM = document.getElementById('SVGcontainer') as HTMLElement
     let svgtxt = this.desc.split('\n')
     let result = ''
-    for (let i in svgtxt) {
-      let elem = this.SVGTrans(svgtxt[i]) as SVGElement
-      result += elem.toSVGItem()
-    }
+    if (this.alertItems.length === 0) {
+      for (let i in svgtxt) {
+        let elem = this.SVGTrans(svgtxt[i]) as SVGElement
+        result += elem.toSVGItem()
+      }
 
-    // console.log(result)
-    DOM.innerHTML = `<svg width="250" height="250" xmlns="http://www.w3.org/2000/svg">${result}</svg>`
+      // console.log(result)
+      DOM.innerHTML = `<svg width="250" height="250" xmlns="http://www.w3.org/2000/svg">${result}</svg>`
+    } else {
+      DOM.innerHTML = `<canvas width="800" id="myCanvas" height="100"
+      style="border:1px solid #d3d3d3;"
+      </canvas>`
+      const canvas = document.getElementById('myCanvas') as HTMLCanvasElement
+      var ctx = canvas.getContext('2d') as CanvasRenderingContext2D
+      ctx.font = '30px Arial'
+      ctx.fillStyle = 'red'
+      ctx.fillText('Errors during parse SVG Items', 10, 50)
+    }
   }
 
   private SVGTrans (txt: string) {
@@ -63,112 +75,12 @@ export default class InputArea extends Vue {
   private alertList (items: Array<string>) {
     let ret: Array<SVGError> = []
     for (let i = 0; i < items.length; i++) {
-      let error = this.errorText(items[i], i)
+      let error = SVGError.readtxtWithErrors(items[i], i)
       if (error as SVGError) {
         ret.push(error as SVGError)
       }
     }
     return ret
-  }
-
-  private errorText (txt: string, line: number) {
-    let itemtxt = txt.split(' ')
-    let index = 0
-    switch (itemtxt[0].toLowerCase()) {
-      case 'r':
-        if (itemtxt.length !== 5) {
-          return new SVGError(
-            'Rectangle should have 4 parameters',
-            line,
-            index + itemtxt[0].length + 1
-          )
-        } else {
-          return this.checkNumber(itemtxt, line, index)
-        }
-      case 'c':
-        if (itemtxt.length !== 4) {
-          console.log(itemtxt.length)
-          return new SVGError(
-            'Circle should have 3 parameters',
-            line,
-            index + itemtxt[0].length + 1
-          )
-        } else {
-          return this.checkNumber(itemtxt, line, index)
-        }
-      case 'e':
-        if (itemtxt.length !== 5) {
-          return new SVGError(
-            'Ellipse should have 4 parameters',
-            line,
-            index + itemtxt[0].length + 1
-          )
-        } else {
-          return this.checkNumber(itemtxt, line, index)
-        }
-      case 'p':
-        if (itemtxt.length < 3) {
-          return new SVGError('Polygon should have more than 3 point', line, txt.length - 1)
-        } else {
-          for (let i = 1; i < itemtxt.length; i++) {
-            index += itemtxt[i - 1].length + 1
-            let demensions = itemtxt[i].split(',')
-            if (demensions.length !== 2) {
-              return new SVGError('Can not divided into 2 demension point', line, index)
-            } else {
-              if (!this.numberValid(demensions[0])) {
-                console.log(demensions[0])
-                return new SVGError('Can not parse x demension', line, index)
-              }
-              if (!this.numberValid(demensions[1])) {
-                return new SVGError('Can not parse y demension', line, index)
-              }
-              return undefined
-            }
-          }
-        }
-        break
-      default: {
-        if (itemtxt.length > 0) {
-          return new SVGError(`The SVG object can not begin with ${itemtxt[0]}`, line, 0)
-        } else {
-          return undefined
-        }
-      }
-    }
-  }
-
-  private checkNumber (itemtxt: Array<string>, line: number, index: number) {
-    const max = 250
-
-    for (let i = 1; i < itemtxt.length; i++) {
-      index += itemtxt[i - 1].length + 1
-      if (!this.numberValid(itemtxt[i])) {
-        return new SVGError('The number is not validat', line, index)
-      }
-      let num = parseInt(itemtxt[i], 10)
-      if (num < 0) {
-        return new SVGError(
-          'The point position below 0 is invalidate',
-          line,
-          index
-        )
-      } else if (num > max) {
-        return new SVGError(
-          `The point position over ${max} is invalidate`,
-          line,
-          index
-        )
-      }
-    }
-    return undefined
-  }
-
-  private numberValid (x: string) {
-    const numberform = new RegExp('^[0-9]+$')
-    if (numberform.test(x)) {
-      return true
-    } else return false
   }
 }
 </script>
--- src/store/types.ts
+++ src/store/types.ts
@@ -11,6 +11,106 @@ export class SVGError {
   toString () {
     return `${this.line}:${this.p}:${this.msg}`
   }
+
+  static readtxtWithErrors (txt: string, line: number) {
+    let itemtxt = txt.split(' ')
+    let index = 0
+    switch (itemtxt[0].toLowerCase()) {
+      case 'r':
+        if (itemtxt.length !== 5) {
+          return new SVGError(
+            'Rectangle should have 4 parameters',
+            line,
+            index + itemtxt[0].length + 1
+          )
+        } else {
+          return SVGError.checkNumber(itemtxt, line, index)
+        }
+      case 'c':
+        if (itemtxt.length !== 4) {
+          console.log(itemtxt.length)
+          return new SVGError(
+            'Circle should have 3 parameters',
+            line,
+            index + itemtxt[0].length + 1
+          )
+        } else {
+          return SVGError.checkNumber(itemtxt, line, index)
+        }
+      case 'e':
+        if (itemtxt.length !== 5) {
+          return new SVGError(
+            'Ellipse should have 4 parameters',
+            line,
+            index + itemtxt[0].length + 1
+          )
+        } else {
+          return SVGError.checkNumber(itemtxt, line, index)
+        }
+      case 'p':
+        if (itemtxt.length < 3) {
+          return new SVGError('Polygon should have more than 3 point', line, txt.length - 1)
+        } else {
+          for (let i = 1; i < itemtxt.length; i++) {
+            index += itemtxt[i - 1].length + 1
+            let demensions = itemtxt[i].split(',')
+            if (demensions.length !== 2) {
+              return new SVGError('Can not divided into 2 demension point', line, index)
+            } else {
+              if (!SVGError.numberValid(demensions[0])) {
+                console.log(demensions[0])
+                return new SVGError('Can not parse x demension', line, index)
+              }
+              if (!SVGError.numberValid(demensions[1])) {
+                return new SVGError('Can not parse y demension', line, index)
+              }
+              return undefined
+            }
+          }
+        }
+        break
+      default: {
+        if (itemtxt.length > 0) {
+          return new SVGError(`The SVG object can not begin with ${itemtxt[0]}`, line, 0)
+        } else {
+          return undefined
+        }
+      }
+    }
+  }
+
+  static checkNumber (itemtxt: Array<string>, line: number, index: number) {
+    const max = 250
+
+    for (let i = 1; i < itemtxt.length; i++) {
+      index += itemtxt[i - 1].length + 1
+      if (!SVGError.numberValid(itemtxt[i])) {
+        return new SVGError('The number is not validat', line, index)
+      }
+      let num = parseInt(itemtxt[i], 10)
+      if (num < 0) {
+        return new SVGError(
+          'The point position below 0 is invalidate',
+          line,
+          index
+        )
+      } else if (num > max) {
+        return new SVGError(
+          `The point position over ${max} is invalidate`,
+          line,
+          index
+        )
+      }
+    }
+    return undefined
+  }
+
+  static numberValid (x: string) {
+    const numberform = new RegExp('^[0-9]+$')
+    if (numberform.test(x)) {
+      return true
+    } else return false
+  }
 }
 
 export class SVGElement {
