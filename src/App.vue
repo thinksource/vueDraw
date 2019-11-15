5209d1a0f3c6601706f4666e706f02a08eb97bfb
thinksource <foretribe@gmail.com>
2019-11-15 12:17:47 +1100

    delete unrelated component

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from './components/HelloWorld.vue'
 import InputArea from './components/InputArea.vue'
 
 @Component({
